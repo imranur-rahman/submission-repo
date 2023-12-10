3ef2653d6f1734d5c3231f1045ff213d72f169f2
Imranur Rahman <ir.shimul@gmail.com>
2023-12-09 22:54:21 -0500

    Can find file pointers with this version.

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ cmake ..
 make
 cd ..
 clang -g -O0 -fpass-plugin=`echo build/skeleton/SkeletonPass.*` test/test1.c
-# echo "test1 completed.\n\n\n\n\n"
-# clang -fpass-plugin=`echo build/skeleton/SkeletonPass.*` test/test2.c
+echo "test1 completed.\n\n\n\n\n"
+clang -g -O0 -fpass-plugin=`echo build/skeleton/SkeletonPass.*` test/test2.c
 # echo "test2 completed.\n\n\n\n\n"
 # clang -g -fpass-plugin=`echo build/skeleton/SkeletonPass.*` test/test3.c
--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -7,17 +7,21 @@
 #include "llvm-14/llvm/IR/DebugInfoMetadata.h"
 #include "llvm-14/llvm/IR/DebugInfo.h"
 
+//#include "llvm-14/llvm/Analysis/DebugInfo.h"
+
 #include "llvm-14/llvm/IR/Instructions.h"
 #include <iostream>
 #include <vector>
 #include <deque>
 #include <map>
+#include <set>
 
 using namespace llvm;
 
 namespace {
 
 std::map<int, std::string>variable_map;
+std::map<Metadata*, std::string>metadata_map;
 
 struct SkeletonPass : public PassInfoMixin<SkeletonPass> {
     PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
@@ -31,11 +35,15 @@ struct SkeletonPass : public PassInfoMixin<SkeletonPass> {
                     if (DbgDeclareInst* ddi = dyn_cast<DbgDeclareInst>(&I)) {
                         errs() << "debug declare inst: " << I << "\n";
                         errs() << ddi->getVariable()->getName() << "\n";
-                        errs() << ddi->getAddress() << "\n";
+                        //errs() << ddi->getAddress() << "\n";
                         const DebugLoc& dl = ddi->getDebugLoc();
                         int line = dl.getLine();
                         errs() << "line no: " << line << "\n";
                         variable_map[line] = ddi->getVariable()->getName();
+
+                        Metadata *Meta = cast<MetadataAsValue>(I.getOperand(0))->getMetadata();
+                        errs() << "metadata: " << *Meta << "\n";
+                        metadata_map[Meta] = ddi->getVariable()->getName();
                     }
                     if (auto* op = dyn_cast<BranchInst>(&I)) {
 
@@ -88,6 +96,7 @@ struct SkeletonPass : public PassInfoMixin<SkeletonPass> {
 
                             // do the analysis here
                             std::deque<Instruction*> worklist;
+                            std::set<Instruction*> visited;
                             for (Use& U: I.operands()) {
                                 Value* v = U.get();
                                 if (Instruction* inst = dyn_cast<Instruction>(v)) {
@@ -103,8 +112,15 @@ struct SkeletonPass : public PassInfoMixin<SkeletonPass> {
                             errs() << "bfs" << "\n";
                             while (worklist.empty() == false) {
                                 Instruction* curr_inst = worklist.front();
-                                errs() << "curr_inst: " << *curr_inst << "\n";
+                                errs() << "\ncurr_inst: " << *curr_inst << "\n";
                                 worklist.pop_front();
+
+                                // if already visited, no need to do the analysis again
+                                if (visited.find(curr_inst) != visited.end()) {
+                                    continue;
+                                }
+                                visited.insert(curr_inst);
+
                                 // base condition
                                 if (AllocaInst* AI = dyn_cast<AllocaInst>(curr_inst)) {
                                     //stack allocation
@@ -112,9 +128,11 @@ struct SkeletonPass : public PassInfoMixin<SkeletonPass> {
                                     // check if any of it's users are scanf
                                     errs() << "users of this allocation instruction: \n";
                                     for (User* user: curr_inst->users()) {
-                                        errs() << *user << "\n";
+                                        // errs() << *user << "\n";
                                         Instruction* user_inst = dyn_cast<Instruction>(user);
                                         errs() << *user_inst << "\n";
+
+                                        // for scanf
                                         if (auto *CI = dyn_cast<CallInst>(user_inst)) {
                                             StringRef callee_name = CI->getCalledFunction()->getName();
                                             errs() << "called function name: " << callee_name << "\n";
@@ -131,16 +149,115 @@ struct SkeletonPass : public PassInfoMixin<SkeletonPass> {
 
                                             }
                                         }
+
+                                        // keep the store instructions to find the load instructions of <store, load> chain
+                                        if (StoreInst* storeInst = dyn_cast<StoreInst>(user_inst)) {
+                                            worklist.push_back(user_inst);
+                                        }
                                     }
                                     errs() << "users of this allocation instruction end \n";
                                 }
+                                if (StoreInst* storeInst = dyn_cast<StoreInst>(curr_inst)) {
+                                    errs() << "found a store instruction\n";
+                                    // TinyPtrVector<DbgDeclareInst *> DIs = FindDbgDeclareUses(storeInst);
+                                    // for (DbgDeclareInst *ddi: DIs) {
+                                    //     dbgs() << "dbg inst: " << *ddi << '\n';
+                                    //     errs() << ddi->getVariable()->getName() << "\n";
+                                    //     const DebugLoc& dl = ddi->getDebugLoc();
+                                    //     int line = dl.getLine();
+                                    //     errs() << "\n\n" << "Line " << line << ": " << ddi->getVariable()->getName() << "\n\n";
+                                    // }
+                                    errs() << "value operand" << *storeInst->getValueOperand() << "\n";
+                                    errs() << "printing operands\n";
+                                    for (Use& U: curr_inst->operands()) {
+                                        errs() << *U << "\n";
+                                    }
 
+                                    // if (Metadata *Meta = cast<MetadataAsValue>(curr_inst->getOperand(0))->getMetadata()) {
+                                    //     errs() << "metadata: " << *Meta << "\n";
+                                    // }
+                                    errs() << "printing metadata\n";
+                                    if (storeInst->hasMetadata()) {
+                                        SmallVector<int>s;
+                                        SmallVector< std::pair< unsigned, MDNode* >> MDs;
+                                        storeInst->getAllMetadata(MDs);
+                                        for (std::pair< unsigned, MDNode* >& it: MDs) {
+                                            errs() << it.first << " " << *it.second << "\n";
+
+                                            // DILocation Loc(it->second);
+                                            // unsigned Line = Loc.getLineNumber();
+                                            // errs() << "Line " << line << ": size of file " << variable_map[line] << "\n";
+                                            errs() << "Metadata ID: " << it.second->getMetadataID() << "\n";
+                                            const DebugLoc* debugLoc = new DebugLoc(it.second);
+                                            unsigned line = debugLoc->getLine();
+                                            errs() << "Line " << line << ": size of file " << variable_map[line] << "\n";
+                                        }
+                                    }
+                                    
+                                }
+                                if (CallInst* callInst = dyn_cast<CallInst>(curr_inst)) {
+                                    errs() << "callInst: " << *callInst << "\n";
+                                    Function* called_function = callInst->getCalledFunction();
+                                    StringRef callee_name = called_function->getName();
+                                    errs() << "called function name: " << callee_name << "\n";
+                                    if (callee_name == "fopen") {
+                                        TinyPtrVector<DbgDeclareInst *> DIs = FindDbgDeclareUses(callInst);
+                                        // There is no debug usage for this case
+                                        for (DbgDeclareInst *ddi: DIs) {
+                                            dbgs() << "dbg inst: " << *ddi << '\n';
+                                            errs() << ddi->getVariable()->getName() << "\n";
+                                            const DebugLoc& dl = ddi->getDebugLoc();
+                                            int line = dl.getLine();
+                                            errs() << "\n\n" << "Line " << line << ": " << ddi->getVariable()->getName() << "\n\n";
+                                        }
+                                        errs() << "args of this callInst\n";
+                                        for (Use& args: callInst->data_ops()) {
+                                            errs() << *args << "\n";
+                                        }
+                                        // errs() << "called func: " << callInst->getCalledFunction()->getOperand(0) << "\n";
+                                        // errs() << "arguments \n";
+                                        // for (Argument& it: called_function->args()) {
+                                        //     errs() << it.dump() << "\n";
+                                        // }
+                                    }
+                                }
+
+                                // Use the load inst of <store, load> chain to find out the seminal input
+                                if (LoadInst* loadInst = dyn_cast<LoadInst>(curr_inst)) {
+                                    errs() << "LoadInst: " << *loadInst << "\n";
+                                    errs() << "users of this load instruction: \n";
+                                    for (User* user: curr_inst->users()) {
+                                        errs() << *user << "\n";
+                                        Instruction* user_inst = dyn_cast<Instruction>(user);
+                                        errs() << *user_inst << "\n";
+                                        // if (auto *CI = dyn_cast<CallInst>(user_inst)) {
+                                        //     StringRef callee_name = CI->getCalledFunction()->getName();
+                                        //     errs() << "called function name: " << callee_name << "\n";
+                                        //     if (callee_name == "__isoc99_scanf") {
+                                        //         TinyPtrVector<DbgDeclareInst *> DIs = FindDbgDeclareUses(AI);
+                                        //         for (DbgDeclareInst *ddi: DIs) {
+                                        //             dbgs() << "dbg inst: " << *ddi << '\n';
+                                        //             errs() << ddi->getVariable()->getName() << "\n";
+                                        //             const DebugLoc& dl = ddi->getDebugLoc();
+                                        //             int line = dl.getLine();
+                                        //             errs() << "\n\n" << "Line " << line << ": " << ddi->getVariable()->getName() << "\n\n";
+                                        //         }
+                                                
+
+                                        //     }
+                                        // }
+                                        worklist.push_back(user_inst);
+                                    }
+                                    errs() << "users of this load instruction end \n";
+                                }
+
+                                errs() << "Add the previous instructions to the worklist.\n";
                                 for (Use& U: curr_inst->operands()) {
-                                    //errs() << *U << "\n";
+                                    errs() << "operands: " << *U << "\n";
                                     Value* v = U.get();
                                     
                                     if (Instruction* prev_inst = dyn_cast<Instruction>(v)) {
-                                        errs() << "next _inst " << " -> " << *prev_inst << "\n";
+                                        errs() << "prev _inst " << " -> " << *prev_inst << "\n";
                                         worklist.push_back(prev_inst);
                                     }
                                 }
